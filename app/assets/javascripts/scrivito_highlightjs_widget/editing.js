c2e9805ee8f9c68f791b090283ee6bdbbc541b4b
Gert Geidel <gert.geidel@infopark.de>
2016-08-22 15:56:53 +0200

    Update editor to editor api

--- app/assets/javascripts/scrivito_highlightjs_widget/editing.js
+++ app/assets/javascripts/scrivito_highlightjs_widget/editing.js
@@ -1,25 +1,25 @@
 (function($, App) {
   'use strict';
 
-  $(function() {
+  var TextAreaEditor = {
+    init_function: function(scrivito_tag) {
+      $(scrivito_tag).on('keyup', function() {
+        $(scrivito_tag).scrivito('save', scrivito_tag.value);
+      });
+    }
+  };
 
-    App.textfieldEditor = {
-      // set selector for Editor
-      selector: '[data-editor=textfield-editor]',
-
-      blurFunction: function(cmsField) {
-        var text = cmsField.val();
-        return cmsField.scrivito('save', text);
-      }
-    };
-
-    scrivito.on('load', function() {
-      return $('body').on('blur', textfieldEditor.selector, function(event) {
-        if(scrivito.in_editable_view()) {
-          textfieldEditor.blurFunction($(event.target));
+  scrivito.on('content', function() {
+    if(scrivito.in_editable_view()) {
+      scrivito.define_editor("text_area_editor", {
+        can_edit: function(element) {
+          return $(element).is('textarea.form-control');
+        },
+        activate: function(element) {
+          TextAreaEditor.init_function(element);
         }
       });
-    });
+    }
   });
 
 })(jQuery, this);
